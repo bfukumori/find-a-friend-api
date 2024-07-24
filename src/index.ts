cee95a514dff3d2b40be2b31dc9f4e3e72f8c1b2
Bruno Fukumori <brunofukumori@gmail.com>
2024-07-24 04:11:48 -0300

    feat: add environment variables

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,22 @@ import fastifyJwt from '@fastify/jwt';
 import { orgRoutes } from '@routes/orgRoutes.js';
 import { petRoutes } from '@routes/petRoutes.js';
 import { fastify } from './@core/infra/libs/fastify.js';
+import { env } from './env.js';
 
 fastify.setErrorHandler(errorHandler);
 
 fastify.register(fastifyJwt, {
-  secret: 'supersecret',
+  secret: env.JWT_SECRET,
 });
 
 fastify.register(petRoutes);
 fastify.register(orgRoutes);
 
-fastify.listen({ port: 3000 }, () => {
-  console.log('Server is running on port 3000');
+fastify.listen({ port: env.PORT }, function (err, address) {
+  if (env.NODE_ENV !== 'production') {
+    console.error(err);
+  }
+  console.log(`Server listening on ${address}`);
 });
 
 export default fastify;
